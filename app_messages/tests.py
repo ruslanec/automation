facf798eaebb92a63019d65fcfaeade3012da016
ruslanec <r.idrisov@gmail.com>
2016-02-08 18:41:11 +0300

    Travis CI teting

--- app_messages/tests.py
+++ app_messages/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class AnimalTestCase(unittest.TestCase):
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(True, True)
